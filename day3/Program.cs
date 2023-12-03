37af524de7db007f56a266e4784fb0396345e01e
Johannes Salminen <johannes.salminen@liaison.com>
2023-12-03 22:23:42 +0200

    add day3 part2

--- day3/Program.cs
+++ day3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Buffers;
+using System.Linq;
 
 namespace day3;
 public class Day3 {
@@ -6,8 +7,10 @@ public class Day3 {
     {
         var input = File.ReadAllLines("./inputs/input");
         char[][] engine = new char[input.Length][];
+        List<(int x, int y, string partNo)> parts = [];
+        List<(int x, int y)> gears = [];
         int part1 = 0;
-        List<int> parts = new();
+        int part2 = 0;
 
         //init schematics array
         for (int i = 0; i < input.Length; i++)
@@ -31,11 +34,14 @@ public class Day3 {
                     }
                 }
                 else {
+                    if(engine[i][j] == '*')
+                    {
+                        gears.Add((i,j));
+                    }
                     if(!string.IsNullOrEmpty(partStr) && isPartNo)
                     {
-                        var p = int.Parse(partStr);
-                        parts.Add(p);
-                        part1 += p;
+                        part1 += int.Parse(partStr);
+                        parts.Add((i, j - partStr.Length, partStr));
                     }
                     partStr = "";
                     isPartNo = false;
@@ -43,21 +49,29 @@ public class Day3 {
             }
             if(!string.IsNullOrEmpty(partStr) && isPartNo)
             {
-                var p = int.Parse(partStr);
-                parts.Add(p);
-                part1 += p;
+                part1 +=int.Parse(partStr);
+                parts.Add((i, engine[i].Length - partStr.Length, partStr));
             }
             partStr = "";
             isPartNo = false;
         }
         if(!string.IsNullOrEmpty(partStr) && isPartNo)
         {
-            var p = int.Parse(partStr);
-            parts.Add(p);
-            part1 += p;
+            part1 += int.Parse(partStr);
+            parts.Add((engine.Length-1, engine[^1].Length - partStr.Length, partStr));
         }
 
         Console.WriteLine($"Part1: {part1}");//530849
+
+        //find * that are gears
+        foreach(var gear in gears) {
+            var adjacentParts = parts.Where(x => (x.x <= gear.x + 1 && x.x >= gear.x - 1) && (x.y >= gear.y - x.partNo.Length && x.y <= gear.y + 1)).ToList();
+            if(adjacentParts.Count == 2) 
+            {
+                part2 += int.Parse(adjacentParts[0].partNo) * int.Parse(adjacentParts[1].partNo);
+            }
+        }
+        Console.WriteLine($"Part2: {part2}");//84900879
     }
     // Helper method to check if char is a number
     const string numbers = "0123456789";
@@ -112,5 +126,86 @@ public class Day3 {
             return true;
         }
         return false;
+    }
+    public static int AdjacentPartNums(int x, int y, char[][] arr) {
+        int ret = 0;
+        //count numbers on top
+        if(x > 0) {
+            int tmp = 0;
+            bool middleIsNumber = false;
+            if(IsNumber(arr[x-1][y-1])) 
+            {
+                tmp++;
+            }
+            if(IsNumber(arr[x-1][y])) 
+            {
+                middleIsNumber = true;
+                tmp++;
+            }
+            if(IsNumber(arr[x-1][y+1])) 
+            {
+                tmp++;
+            }
+            if(tmp == 3)
+            {
+                ret++;
+            }
+            else if(tmp == 2)
+            {
+                if(middleIsNumber) {
+                    ret++;
+                }
+                else  {
+                    ret += 2;
+                }
+            }
+            else {
+                ret += tmp;
+            }
+        }
+        //count same row
+        if(y > 0 && IsNumber(arr[x][y-1]))
+        {
+            ret++;
+        }
+        if(y < arr[x].Length - 1 && IsNumber(arr[x][y+1]))
+        {
+            ret++;
+        }
+        //count bottom row
+        if(x < arr.Length - 1) {
+            int tmp = 0;
+            bool middleIsNumber = false;
+            if(y > 0 && IsNumber(arr[x+1][y-1])) 
+            {
+                tmp++;
+            }
+            if(IsNumber(arr[x+1][y])) 
+            {
+                middleIsNumber = true;
+                tmp++;
+            }
+            if(y < arr[x].Length - 1 && IsNumber(arr[x+1][y+1])) 
+            {
+                tmp++;
+            }
+            if(tmp == 3)
+            {
+                ret++;
+            }
+            else if(tmp == 2)
+            {
+                if(middleIsNumber) {
+                    ret++;
+                }
+                else  {
+                    ret += 2;
+                }
+            }
+            else {
+                ret += tmp;
+            }
+        }
+        return ret;
     }
 }
