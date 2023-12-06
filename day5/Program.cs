64ddfcc9f3b003a50b299b3383c4c6dd94768c2c
Johannes Salminen <jsalminen@opentext.com>
2023-12-06 09:59:54 +0200

    add day5

--- day5/Program.cs
+++ day5/Program.cs
@@ -2,17 +2,23 @@
 
 public class Day5 {
     public static void Main() 
-    { /*
-        var input = File.ReadAllLines("inputs/test");
+    {
+        var input = File.ReadAllLines("inputs/input");
         var seeds = input[0].Split(": ")[1].Split(" ");
+        bool[] found = new bool[seeds.Length];
         for(int i = 1; i < input.Length; i++)
         {
             if(string.IsNullOrEmpty(input[i]))
             {
                 //old map ends
+                Console.WriteLine("[{0}]", string.Join(", ", seeds));
+                for(int f = 0; f < found.Length; f++) {
+                    found[f] = false;
+                }
             }
             else if(input[i].Contains("map")) {
                 //new map starts
+                Console.WriteLine(input[i]);
             }
             else {
                 //map row
@@ -20,25 +26,63 @@ public class Day5 {
                 var map = input[i].Split(" ");
                 for(var s = 0; s < seeds.Length; s++)
                 {
-                    seeds[s] = ConvertSeed(seeds[s], map);
+                    if(!found[s]) {
+                        seeds[s] = ConvertSeed(seeds[s], map, ref found[s]);
+                    }
                 }
             }
         }
-        
+        Console.WriteLine("[{0}]", string.Join(", ", seeds));
         Console.WriteLine("Part1: " + seeds.Min());
-        */
-        Console.WriteLine();
     }
-    private static string ConvertSeed(string seed, string[] map)
+    private static string ConvertSeed(string seed, string[] map, ref bool found)
     {
-        return "";
+        //check if seed is >= destination start
+        if (IsGreater(seed, map[1]) != -1 )
+        {
+            //subtract destinations start from original seed
+            var subtraction = SubtractStr(seed,map[1]);
+            //check if destination range is larger than the substraction (= is the seed in the range)
+            if(IsGreater(map[2],subtraction) == 1) {
+                //seed was in the range, mark as found and 
+                found = true;
+                return AddStr(map[0], subtraction);
+            }
+        }
+        return seed;
     }
     public static string SubtractStr(string value, string subtractor)
     {
-        //todo
+        if (subtractor.Length > value.Length)
+            throw new NotImplementedException();
+        if (value.Equals(subtractor))
+            return "0";
+        var result = "";
+        var a = value.ToCharArray();
+        var b = subtractor.ToCharArray();
+        for(int i = 0; i < a.Length; i++)
+        {   
+            if(i < b.Length) 
+            {
+                if (a[a.Length - i - 1] < b[b.Length - i -1])
+                {
+                    a[a.Length - i - 1] += (char)10;
+                    a[a.Length - i - 2]--;
+                }
+                result = a[a.Length - i - 1] - b[b.Length - i -1] + result;
+            }
+            else {
+                result = a[a.Length - i - 1] + result;
+            }
+        }
+        return result.TrimStart('0');
     }
     public static int IsGreater(string value, string compare) {
-        if(value.Length > compare.Length) 
+        if(value.Equals(compare))
+        {
+            return 0;
+        }
+        else if(value.Length > compare.Length) 
         {
             return 1;
         }
@@ -77,14 +121,14 @@ public class Day5 {
                 init[init.Length - 1 - i]++;//works?
                 overflow = false;
             }
-            Console.WriteLine(init[init.Length - 1 - i] + " + " + add[add.Length - 1 - i]);
+            //Console.WriteLine(init[init.Length - 1 - i] + " + " + add[add.Length - 1 - i]);
             var digit = (char)(init[init.Length - 1 - i] + add[add.Length - 1 - i] - '0');
             if(digit > '9') {
                 overflow = true;
                 digit = (char)('0' + (digit - ':'));
             }
             result = digit + result;
-            Console.WriteLine("result "+result);
+            //Console.WriteLine("result "+result);
         }
         for(int i = init.Length - add.Length - 1; i >= 0; i--) {
             if(overflow) {
@@ -96,13 +140,13 @@ public class Day5 {
                 init[i] = (char)('0' + (init[i] - ':'));
             }
             result = init[i] + result;
-            Console.WriteLine("result "+result);
+            //Console.WriteLine("result "+result);
         }
         if(overflow)
         {
             result = '1' + result;
         }
         
-        return result;
+        return result.TrimStart('0');
     }
 }
