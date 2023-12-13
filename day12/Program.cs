fe507905a043c71417ef2b15aa4a5c635064556a
Johannes Salminen <jsalminen@opentext.com>
2023-12-13 16:46:30 +0200

    add day 12 and started day 13

---
+++ day12/Program.cs
@@ -0,0 +1,69 @@
+﻿public class Day12
+{
+    private static void Main(string[] args)
+    {
+        var input = File.ReadAllLines("inputs/test");
+        int part1 = 0;
+        foreach (var inputLine in input)
+        {
+            var line = inputLine.Split(" ");
+            var springs = line[0];
+            var groups = line[1].Split(",");
+
+            part1 += Solve(springs, groups);
+        }
+        Console.WriteLine("Part1: " + part1);//6958
+
+        int part2 = 0;
+        int row = 1;
+        foreach (var inputLine in input)
+        {
+            var line = inputLine.Split(" ");
+            var springs = line[0] + "?" + line[0] + "?" + line[0] + "?" + line[0] + "?" + line[0];
+            var groups = (line[1] + "," + line[1] + "," + line[1] + "," + line[1] + "," + line[1]).Split(",");
+
+            part2 += Solve(springs, groups); // too slow :(
+            Console.WriteLine(row);
+            row++;
+        }
+        Console.WriteLine("Part2: " + part2);
+    }
+    private static int Solve(string springs, string[] groups) {
+        List<string> combinations = [springs];
+
+            for (int i = 0; i < springs.Length; i++)
+            {
+                if (springs[i] == '?')
+                {
+                    List<string> additions = [];
+                    for (int j = 0; j < combinations.Count; j++)
+                    {
+                        char[] s = combinations[j].ToCharArray();
+                        s[i] = '#';
+                        additions.Add(new string(s));
+                        s[i] = '.';
+                        additions.Add(new string(s));
+                    }
+                    combinations = additions;
+                }
+            }
+            int valid = 0;
+            foreach (var c in combinations)
+            {
+                //Console.WriteLine(c);
+                var t = c.Split(".", StringSplitOptions.RemoveEmptyEntries);
+                bool validB = t.Length == groups.Length;
+                for (int i = 0; validB && i < t.Length; i++)
+                {
+                    if (t[i].Length != int.Parse(groups[i]))
+                    {
+                        validB = false;
+                        break;
+                    }
+                }
+                if (validB)
+                    valid++;
+            }
+            return valid;
+    }
+}
---
+++ day13/Program.cs
@@ -0,0 +1,76 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var input = File.ReadAllLines("inputs/test");
+        int start = -1;
+        int part1 = 0;
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (string.IsNullOrEmpty(input[i]))
+            {
+                //check  reflections
+                part1 += FindReflection(start, i-1, ref input);
+                //start new mirror on next row
+                start = -1;
+            }
+            else if (start == -1)
+            {
+                start = i;
+            }
+            else {
+
+            }
+        }
+        //check last
+        part1 += FindReflection(start, input.Length-1, ref input);
+    }
+    private static int FindReflection(int start, int end, ref string[] input) {
+        for(int i = start; i < end; i++)
+        {
+            bool isReflection = input[i].Equals(input[i+1]);
+            int test = i - 1;
+            int testEnd = i + 2;
+            while(isReflection && test >= start && testEnd <= end)
+            {
+                isReflection = input[test].Equals(input[testEnd]);
+                test--;
+                testEnd++;
+            }
+            if(isReflection)
+            {
+                Console.WriteLine("Horizontal reflection at line " + (i - start));
+                return i;
+            }
+        }
+        List<string> columns = [];
+        for(int i = 0; i < input[start].Length - 1; i++)
+        {
+            string column = "";
+            for(int j = 0; j < end; j++)
+            {
+                column += input[j][i];
+
+            }
+            columns.Add(column);
+        }
+        for(int i = 0; i < columns.Count - 1; i++)
+        {
+            bool isReflection = columns[i].Equals(columns[i+1]);
+            int test = i - 1;
+            int testEnd = i + 2;
+            while(isReflection && test >= 0 && testEnd < columns.Count)
+            {
+                isReflection = columns[test].Equals(columns[testEnd]);
+                test--;
+                testEnd++;
+            }
+            if(isReflection)
+            {
+                Console.WriteLine("Vertical reflection at column " + i);
+                return i;
+            }
+        }
+        return 0;       
+    }
+}
