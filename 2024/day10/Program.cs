f03a9def3e3f3879d99c46b1ac2f60f64a4ccb5c
Johannes Salminen <jsalminen@opentext.com>
2024-12-10 10:35:52 +0200

    tidy day10

--- 2024/day10/Program.cs
+++ 2024/day10/Program.cs
@@ -2,28 +2,35 @@
 {
     private static void Main()
     {
-        var input = File.ReadAllLines("input");
+        var map = File.ReadAllLines("input");
+        var trailhead = '0';
+        var trailend = '9';
+
         int part1 = 0;
         int part2 = 0;
-        for (int i = 0; i < input.Length; i++) 
+
+        for (int i = 0; i < map.Length; i++) 
         {
-            for (int j = 0; j < input[0].Length; j++)
+            for (int j = 0; j < map[0].Length; j++)
             {
-                if (input[i][j] == '0')
+                // Find trailheads
+                if (map[i][j] == trailhead)
                 {
-                    //Console.WriteLine($"Trailhead at {i} {j}");
-                    part1 += Score(input, (i,j)).Count;
-                    part2 += Rating(input, (i,j));
+                    // Score = number of unique trailends accessible from trailhead
+                    part1 += GetTrailEnds(map, (i,j), trailend).Count;
+                    // Rating = number on different routes from trailhead to a trailend
+                    part2 += Rating(map, (i,j), trailend);
                 }
             }
         }
         Console.WriteLine(part1);
         Console.WriteLine(part2);
     }
-    private static Dictionary<(int,int), bool> Score(string[] map, (int i, int j) p)
+    // 
+    private static Dictionary<(int,int), bool> GetTrailEnds(string[] map, (int i, int j) p, char trailend)
     {
         Dictionary<(int,int), bool> ends = [];
-        if(map[p.i][p.j] == '9')
+        if(map[p.i][p.j] == trailend)
         {
             ends[p] = true;
             return ends;
@@ -31,7 +38,7 @@
         else {
             foreach (var n in Next(map, p))
             {
-                foreach(var end in Score(map, n).Keys)
+                foreach(var end in GetTrailEnds(map, n, trailend).Keys)
                 {
                     ends[end] = true;
                 }
@@ -39,9 +46,9 @@
             return ends;
         }
     }
-    private static int Rating(string[] map, (int i, int j) p)
+    private static int Rating(string[] map, (int i, int j) p, char trailend)
     {
-        if(map[p.i][p.j] == '9')
+        if(map[p.i][p.j] == trailend)
         {
             return 1;
         }
@@ -49,45 +56,30 @@
             int rating = 0;
             foreach (var n in Next(map, p))
             {
-                rating += Rating(map, n);
+                rating += Rating(map, n, trailend);
             }
             return rating;
         }
     }
+    // Get list of neighbours for point p
     private static List<(int i, int j)> Next(string[] map, (int i, int j) p)
     {
         List<(int i, int j)> result = [];
-        if(p.i > 0 )
+        if(p.i > 0 && map[p.i-1][p.j] - map[p.i][p.j] == 1)
         {
-            var diff = map[p.i-1][p.j] - map[p.i][p.j];
-            if(diff == 1)
-            {
-                result.Add((p.i-1, p.j));
-            }
+            result.Add((p.i-1, p.j));
         }
-        if(p.j > 0 )
+        if(p.j > 0 && map[p.i][p.j-1] - map[p.i][p.j] == 1)
         {
-            var diff = map[p.i][p.j-1] - map[p.i][p.j];
-            if(diff == 1)
-            {
-                result.Add((p.i, p.j-1));
-            }
+            result.Add((p.i, p.j-1));
         }
-        if(p.i < map.Length-1)
+        if(p.i < map.Length-1 && map[p.i+1][p.j] - map[p.i][p.j] == 1)
         {
-            var diff = map[p.i+1][p.j] - map[p.i][p.j];
-            if(diff == 1)
-            {
-                result.Add((p.i+1, p.j));
-            }
+            result.Add((p.i+1, p.j));
         }
-        if(p.j < map[0].Length-1)
+        if(p.j < map[0].Length-1 && map[p.i][p.j+1] - map[p.i][p.j] == 1)
         {
-            var diff = map[p.i][p.j+1] - map[p.i][p.j];
-            if(diff == 1)
-            {
-                result.Add((p.i, p.j+1));
-            }
+            result.Add((p.i, p.j+1));
         }
         return result;
     }
