9a51b0528357620b03a073f40087615fce4a3035
Johannes Salminen <jsalminen@opentext.com>
2024-12-11 07:59:28 +0200

    add day11 naive

---
+++ 2024/day11/Program.cs
@@ -0,0 +1,54 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        var stones = File.ReadAllLines("input")[0].Split(" ").ToList();
+        for (int i = 0; i < 25; i++)
+        {
+            //Console.WriteLine(string.Join(" ", stones));
+            stones = Blink(stones);
+        }
+        Console.WriteLine(stones.Count);
+        for (int i = 25; i < 75; i++)
+        {
+            //Console.WriteLine(string.Join(" ", stones));
+            stones = Blink(stones);
+        }
+        Console.WriteLine(stones.Count);
+    }
+    private static List<string> Blink(List<string> stones)
+    {
+        List<string> result = [];
+        foreach (var stone in stones)
+        {
+            if(stone == "0")
+            {
+                result.Add("1");
+            }
+            else if(stone.Length % 2 == 0)
+            {
+                var tmp = stone[..(stone.Length / 2)].TrimStart('0');
+                if (string.IsNullOrEmpty(tmp))
+                {
+                    result.Add("0");
+                }
+                else {
+                    result.Add(tmp);
+                }
+                tmp = stone[(stone.Length / 2)..].TrimStart('0');
+                if (string.IsNullOrEmpty(tmp))
+                {
+                    result.Add("0");
+                }
+                else {
+                    result.Add(tmp);
+                }
+            }
+            else {
+                var tmp = long.Parse(stone) * 2024;
+                result.Add(tmp.ToString());
+            }           
+        }
+        return result;
+    }
+}
