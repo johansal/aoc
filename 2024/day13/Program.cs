d54226712fc978d48b376a2ab9c7ab79a5ca8a39
Johannes Salminen <jsalminen@opentext.com>
2024-12-13 16:41:33 +0200

    day13 tidy part1

--- 2024/day13/Program.cs
+++ 2024/day13/Program.cs
@@ -3,39 +3,29 @@
     private static void Main(string[] args)
     {
         var input = File.ReadLines("input");
-        (int x, int y) buttonA = (0,0);
-        (int x, int y) buttonB = (0,0);
-        (int x, int y) prize = (0,0);
+        (int x, int y) a = (0,0);
+        (int x, int y) b = (0,0);
         int part1 = 0;
         foreach (var line in input)
         {
-            if(string.IsNullOrEmpty(line))
-            {
-                // Do nothing.
-            }
-            else if (line.StartsWith("Button")) {             
+            if (line.StartsWith("Button")) {             
                 var spl = line.Split(" ");
                 var x = Parse(spl[2]);
                 var y = Parse(spl[3]);
                 if (spl[1] == "A:")
                 {
-                    buttonA = (x,y);
+                    a = (x,y);
                 }
                 else if (spl[1] == "B:"){
-                    buttonB = (x,y);
-                }
-                else {
-                    throw new Exception("Parsing error!");
+                    b = (x,y);
                 }
             }
-            else {
+            else if (line.StartsWith("Prize")) {
                 var spl = line.Split(" ");
                 var x = Parse(spl[1]);
                 var y = Parse(spl[2]);
-                prize = (x,y);
-
                 // Solve
-                int cheapest = Solve(buttonA, buttonB, prize);
+                int cheapest = Solve(a, b, (x,y));
                 if (cheapest != int.MaxValue)
                 {
                     part1 += cheapest;
@@ -50,23 +40,18 @@
             return int.Parse(value[2..^1]);
         return int.Parse(value[2..]);
     }
-    private static int Solve((int x, int y) a, (int x, int y) b, (int x, int y) prize)
+    private static int Solve((int i, int j) a, (int i, int j) b, (int i, int j) p)
     {
-        //Console.WriteLine($"Solving: A {a.x},{a.y}; B {b.x},{b.y}; Prize {prize.x},{prize.y}");
-        int cheapest = int.MaxValue;
-        for (int i = 1; i <= 100; i++)
+        int min = int.MaxValue;
+        for (int x = 1; x <= 100; x++)
         {
-            for (int j = 1; j <= 100; j++)
+            if((a.j * x) + (b.j * ((p.i - a.i * x)/b.i)) == p.j)
             {
-                if((a.x * i) + (b.x * j) == prize.x &&
-                (a.y * i) + (b.y * j) == prize.y)
-                {
-                    int value = i * 3 + j;
-                    if(value < cheapest)
-                        cheapest = value;
-                }
+                int value = x * 3 + ((p.i - (a.i * x)) / b.i);
+                if(value < min)
+                    min = value;
             }
         }
-        return cheapest;
+        return min;
     }
 }
