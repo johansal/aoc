7e619bb7e463cbbcf4fdee2617d9b33b36261995
Johannes Salminen <jsalminen@opentext.com>
2024-12-19 09:30:42 +0200

    another impl. for d19 part1

--- 2024/day19/Program.cs
+++ 2024/day19/Program.cs
@@ -1,27 +1,37 @@
-﻿using System.Reflection.Metadata.Ecma335;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main()
     {
         var input = File.ReadAllLines("input");
         var patterns = input[0].Split(", ");
         var part1 = 0;
+        var part2 = 0;
 
         for(int i = 2; i < input.Length; i++)
         {
             var design = input[i];
-            if(Match(design, patterns))
-            {
-                //Console.WriteLine(design);
-                part1++;
-            }
+            Console.WriteLine($"cheking: {design}");
+            var ret = FindAll(design, patterns, 0);
+            part1 += ret == 0 ? 0 : 1;
+            part2 += ret;
         }
         Console.WriteLine(part1);
+        Console.WriteLine(part2);
     }
-    private static bool Match(string design, string[] patterns)
+    private static int FindAll(string design, IEnumerable<string> patterns, int matches) {
+        var partial = patterns.Where(pattern => design.Length >= pattern.Length && design.StartsWith(pattern));
+        var exact = partial.Where(pattern => design.Length == pattern.Length).Count();
+        foreach(var pattern in partial)
+        {
+            var part = design.Substring(pattern.Length);
+            exact = FindAll(part, patterns, exact);
+        }
+        return exact + matches;
+    }
+/*
+    private static int Match(string design, string[] patterns)
     {
-        var partialMatches = PartialMatch(design, 0, patterns);
+        var partialMatches = PartialMatch(design, patterns);
         while(partialMatches.Where(m => m < design.Length).Any())
         {
             List<int> m = partialMatches.Where(m => m == design.Length).ToList();
@@ -32,21 +42,21 @@ internal class Program
             }
             foreach (var item in partialMatches.Where(m => m < design.Length))
             {
-                m = m.Union(PartialMatch(design, item, patterns)).ToList();
+                m = m.Concat(PartialMatch(design, item, patterns)).ToList();
             }
             partialMatches = m;
         }
-        return partialMatches.Where(m => m == design.Length).Any();
+        return partialMatches.Where(m => m == design.Length).Count();
     }
-    private static List<int> PartialMatch(string design, int position, string[] patterns) {
-        var part = design.Substring(position);
+    private static List<int> PartialMatch(string design, string[] patterns) {
         List<int> possible = [];
         //Console.WriteLine($"cheking: {part}");
         for(int i = 0; i < patterns.Length; i++)
         {
-            if(part.Length >= patterns[i].Length && part.StartsWith(patterns[i]))
-                possible.Add(position + patterns[i].Length);
+            if(design.Length >= patterns[i].Length && design.StartsWith(patterns[i]))
+                possible.Add(i);
         }
         return possible;
     }
+*/
 }
