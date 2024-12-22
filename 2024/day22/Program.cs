181c3e077d4fc058f7d943ce9824a4f467089644
unknown <hawktub@gmail.com>
2024-12-22 11:28:46 +0200

    add day22

--- 2024/day22/Program.cs
+++ 2024/day22/Program.cs
@@ -4,17 +4,44 @@
     {
         var input = File.ReadAllLines("input");
         ulong part1 = 0;
+        Dictionary<string, int> seqPrice = [];
         foreach(var line in input)
         {
+            HashSet<string> sequences = [];
+            List<int> sequence = [];
             ulong secret = ulong.Parse(line);
+            // price is the last digit of the "random" secret
+            int price = (int)(secret % 10);
             for(int i = 0; i < 2000; i++)
             {
                 secret = GetSecret(secret);
+                int newPrice = (int)(secret % 10);
+                int change = newPrice - price;
+                price = newPrice; 
+                // create price change sequences of 4, 
+                // save price if this was the first time seq was encountered for this monkey
+                // if it was, add the price to total for this sequence
+                sequence.Add(change);
+                if(i > 3)
+                    sequence.RemoveAt(0);
+                if (i >= 3)
+                {
+                    var hash = string.Join(",", sequence);
+                    if(sequences.Add(hash))
+                    {
+                        if(seqPrice.ContainsKey(hash))
+                            seqPrice[hash] += price;
+                        else
+                            seqPrice[hash] = price;
+                        //Console.WriteLine($"New seq {hash}: {price}");
+                    }
+                }
             }
             part1 += secret;
             //Console.WriteLine($"{line}: {secret}");
         }
         Console.WriteLine(part1);
+        Console.WriteLine(seqPrice.Values.Max());
         
     }
     private static ulong GetSecret(ulong secret)
