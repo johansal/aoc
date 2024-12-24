5eb9e77880a20bf45cee67a3858528140f7dc08d
unknown <hawktub@gmail.com>
2024-12-24 11:02:18 +0200

    start day24 part1

---
+++ 2024/day24/Program.cs
@@ -0,0 +1,54 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var input = File.ReadAllLines("test");
+        Dictionary<string,bool> wires = [];
+        List<string[]> backlog = [];
+
+        bool isInit = true;
+        foreach(var line in input)
+        {
+            if(string.IsNullOrEmpty(line))
+                isInit = false;
+            else if(isInit)
+            {
+                var w = line.Split(": ");
+                wires[w[0]] = w[1] == "1";
+            }
+            else {
+                // simulate 'x00 AND y00 -> z00'
+                var w = line.Split(' ');
+                if(wires.ContainsKey(w[0]) && wires.ContainsKey(w[2]))
+                {
+                    wires[w[4]] = Logic(wires[w[0]], w[1], wires[w[2]]);
+                }
+                else {
+                    backlog.Add(w);
+                }
+            }
+        }
+        while(backlog.Count != 0)
+        {
+            foreach(var w in backlog)
+            {
+                if(wires.ContainsKey(w[0]) && wires.ContainsKey(w[2]))
+                {
+                    wires[w[4]] = Logic(wires[w[0]], w[1], wires[w[2]]);
+                    backlog.Remove(w);
+                }
+            }
+        }
+    }
+    private static bool Logic(bool wire, string port, bool wire2)
+    {
+        var ret  = port switch
+        {
+            "AND" => wire && wire2,
+            "OR" => wire || wire2,
+            "XOR" => wire ^ wire2,
+            _ => throw new Exception("invalid operand"),
+        };
+        return ret;
+    }
+}
