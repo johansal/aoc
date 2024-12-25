3b70a3b4f0c7476f0d0582f14ce2df1a844c2a0d
Johannes Salminen <jsalminen@opentext.com>
2024-12-25 11:11:05 +0200

    add day 25

---
+++ 2024/day25/Program.cs
@@ -0,0 +1,79 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var input = File.ReadAllLines("input");
+
+        List<List<int>> keys = [];
+        List<List<int>> locks = [];
+
+        List<string> current = [];
+        List<int> tmp = [];
+        foreach(var line in input)
+        {
+            if(string.IsNullOrEmpty(line))
+            {
+                tmp = HeightMap(current[1..^1]);
+                Console.WriteLine(string.Join(",", tmp));
+                if(current[0][0] == '.')
+                {
+                    //key
+                    keys.Add(tmp);
+                }
+                else {
+                    //lock
+                    locks.Add(tmp);
+                }
+                current = [];
+            }
+            else {
+                current.Add(line);
+            }
+        }
+        tmp = HeightMap(current[1..^1]);
+        Console.WriteLine(string.Join(",", tmp));
+        if(current[0][0] == '.')
+        {
+            //key
+            keys.Add(tmp);
+        }
+        else {
+            //lock
+            locks.Add(tmp);
+        }
+
+        var part1 = 0;
+        foreach(var key in keys)
+        {
+            foreach(var loc in locks)
+            {
+                bool fits = true;
+                for(int i = 0; i < 5; i++)
+                {
+                    if(loc[i] > 5 - key[i])
+                    {
+                        fits = false;
+                        break;
+                    }
+                }
+                if(fits)
+                    part1++;
+            }
+        }
+        Console.WriteLine(part1); 
+    }
+    private static List<int> HeightMap(List<string> input) {
+        List<int> heights = [0,0,0,0,0];
+        for(int i = 0; i < input.Count; i++)
+        {
+            for(int j = 0; j < input[i].Length; j++)
+            {
+                if(input[i][j] == '#')
+                {
+                    heights[j]++;
+                }
+            }
+        }
+        return heights;
+    }
+}
