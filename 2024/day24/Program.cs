b8f5dbbee5b883600474aef25b5fb54fa4c51f0a
unknown <hawktub@gmail.com>
2024-12-28 14:11:09 +0200

    day24 part 2 kind of...

--- 2024/day24/Program.cs
+++ 2024/day24/Program.cs
@@ -5,6 +5,7 @@
         var input = File.ReadAllLines("input");
         Dictionary<string,bool> wires = [];
         List<string[]> backlog = [];
+        Dictionary<string, string> connections = [];
 
         bool isInit = true;
         foreach(var line in input)
@@ -19,6 +20,8 @@
             else {
                 // simulate 'x00 AND y00 -> z00'
                 var w = line.Split(' ');
+                var wPart2 = line.Split(" -> ");
+                connections[wPart2[1]] = wPart2[0];
                 if(wires.ContainsKey(w[0]) && wires.ContainsKey(w[2]))
                 {
                     wires[w[4]] = Logic(wires[w[0]], w[1], wires[w[2]]);
@@ -30,7 +33,6 @@
         }
         while(backlog.Count != 0)
         {
-            Console.WriteLine($"Backlog size {backlog.Count}");
             List<string[]> done = [];
             foreach(var w in backlog)
             {
@@ -49,9 +51,32 @@
         foreach(var w in wires.Keys.Where(x => x.StartsWith('z')).OrderByDescending(x => x))
         {
             //Console.WriteLine($"{w} {wires[w]}");
+            Console.Write(wires[w] ? 1UL : 0UL);
             part1 = (part1 << 1) | (wires[w] ? 1UL : 0UL);
         }
+        Console.Write("\n");
         Console.WriteLine(part1);
+        
+        //part2
+        //no clever solution, just dts the connections and print to console, find anomalies in normal addition by hand and swap to correct.
+        Dictionary<string, string> visited = [];
+        for(int i = 0; i < 45; i++)
+        {
+            string port = "z";
+            port += i < 10 ? "0"+i : i;
+            Console.WriteLine($"{port} is connected to {Trace(port, connections, visited)}");
+        }
+        string[] part2 = ["z31","dmh","z38","dvq","z11","rpv","ctg","rpb"];
+        Console.WriteLine(string.Join(",",part2.Order()));
+    }
+    private static string Trace(string key, Dictionary<string, string> connections, Dictionary<string, string> visited) 
+    {
+        var c = connections[key];
+        var next = c.Split(" ");
+        var n0 = ((next[0].StartsWith('x') || next[0].StartsWith('y') || visited.ContainsKey(key)) == false) ? Trace(next[0], connections, visited) : next[0];
+        var n2 = ((next[2].StartsWith('x') || next[2].StartsWith('y') || visited.ContainsKey(key)) == false) ? Trace(next[2], connections, visited) : next[2];
+        visited[key] = c;
+        return $"({n0} {next[1]} {n2})={key}";
     }
     private static bool Logic(bool wire, string port, bool wire2)
     {
